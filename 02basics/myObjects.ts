f4815d4416ba6491f10a1fe0f34ae004125aed6b
Abhi Nair <abhilashnair1994@gmail.com>
2023-08-02 11:05:24 -0600

    variables, objects and functions

--- 02basics/myObjects.ts
+++ 02basics/myObjects.ts
@@ -1,50 +1,55 @@
 // const User = {
-//     name: "hitesh",
-//     email: "hitesh@lco.dev",
-//     isAvtive: true
+//     name: "Abhi",
+//     // email: "abhi@gmail.com",
+//     // isActive: true,
+//     isPaid: true
 // }
 
 // function createUser({name: string, isPaid: boolean}){}
 
-// let newUser = {name: "hitesh", isPaid: false, email: "h@h.com"}
+
+let newUser = {name: "hitesh", isPaid: false, email: "h@h.com"}
 
 // createUser(newUser)
 
 
 
-// function createCourse():{name: string, price: number}{
-//     return {name: "reactjs", price: 399}
-// }
+function createCourse():{name: string, price: number}{
+    return {name: "reactjs", price: 399}
+}
+
+console.log(createCourse())
+
+
+// create types
+type User = {
+    name: string;
+    email: string;
+    isActive: boolean
+}
+
+
+function createUser(user: User): User{
+    return {name: user.name, email: user.email, isActive: user.isActive}
+}
+
+console.log(createUser({name: "Abhi", email: "abhilashnair1994@gmail.com", isActive: true}))
 
 
 // type User = {
-//     name: string;
-//     email: string;
+//     readonly _id: string
+//     name: string
+//     email: string
 //     isActive: boolean
+//     credcardDetails?: number
 // }
 
-
-// function createUser(user: User): User{
-//     return {name: "", email: "", isActive: true}
+// let myUser: User = {
+//     _id: "1245",
+//     name: "h",
+//     email: "h@h.com",
+//     isActive: false
 // }
-
-// createUser({name: "", email: "", isActive: true})
-
-
-type User = {
-    readonly _id: string
-    name: string
-    email: string
-    isActive: boolean
-    credcardDetails?: number
-}
-
-let myUser: User = {
-    _id: "1245",
-    name: "h",
-    email: "h@h.com",
-    isActive: false
-}
 
 type cardNumber = {
     cardnumber: string
@@ -59,7 +64,7 @@ type cardDetails = cardNumber & cardDate & {
 }
 
 
-myUser.email = "h@gmail.com"
+// myUser.email = "h@gmail.com"
 // myUser._id = "asa"
 
 
--- 02basics/myfunctions.ts
+++ 02basics/myfunctions.ts
@@ -1,15 +1,23 @@
+//infer number for parameter and return type
 function addTwo(num: number): number{
      return num + 2
-    //return "hello"
+     //return "hello"
 }
+
+addTwo(5)
 
 function getUpper(val: string){
     return val.toUpperCase()
 }
 
+console.log(getUpper('4'))
+
 function signUpUser(name: string, email: string, isPaid: boolean){}
 
-let loginUser = (name: string, email: string, isPaid: boolean = false) => {}
+// Arrow functions also include default types
+let loginUser = (name: string, email: string, isPaid: boolean = false) => {
+    if (isPaid === void 0) {isPaid = false; }
+}
 
 let myValue = addTwo(5)
 getUpper("hitesh")
@@ -24,27 +32,32 @@ loginUser("h", "h@h.com")
 //     return "200 OK"
 // }
 
+// arrow functions
 const getHello = (s: string):string => {
     return ""
 }
 
 const heros = ["thor", "spiderman", "ironman"]
-// const heros = [1, 2, 3]
+//const heros = [1, 2, 3]
 
 heros.map((hero): string => {
     return `hero is ${hero}`
 })
 
-
+// void doesn't return anything
 function consoleError(errmsg: string): void{
     console.log(errmsg);
     
 }
+// throws the error
+// need to reach an endpoint
 function handleError(errmsg: string): never{
     throw new Error(errmsg);
     
 }
 
+consoleError("the error")
+//handleError("the error")
 
 
 export {}
