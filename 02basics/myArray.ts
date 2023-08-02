7c03e00deeda7ec3a9a88116114075f6fb9526a8
Abhi Nair <abhilashnair1994@gmail.com>
2023-08-02 11:24:39 -0600

    arrays

--- 02basics/myArray.ts
+++ 02basics/myArray.ts
@@ -1,13 +1,32 @@
+// does not work
+// const superHeros = []
+// superHeros.push('hi')
+// also returns never
+// const superHeros: [] = []
+
 const superHeros: string[] = []
-// const heroPower: number[] = []
+superHeros.push('spiderMan')
+console.log(superHeros)
+
+// array  example
+//const heroPower: number[] = []
+
+
+// alternate
 const heroPower: Array<number> = []
+
+heroPower.push(2)
+console.log(heroPower)
 
 type User = {
     name: string
     isActive: boolean
 }
 
-const allUsers: User[] = []
+//const allUsers: User[] = []
+const allUsers: Array<User> = []
+allUsers.push({name: "", isActive:true})
+console.log(allUsers)
 
 const MLModels: number[][] = [
     [255, 255, 255],
@@ -15,7 +34,7 @@ const MLModels: number[][] = [
 ]
 
 
-superHeros.push("spiderman")
-heroPower.push(2)
+// superHeros.push("spiderman")
+// heroPower.push(2)
 
-allUsers.push({name: "", isActive: true})
+// allUsers.push({name: "", isActive: true})
